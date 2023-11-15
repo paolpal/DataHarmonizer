8adddad473ca84ee504c5ffc0fc4eb2b7378ad40
paolo-asus <paol.palumbo@gmail.com>
2023-11-15 15:28:19 +0100

    scritto un filtraggio per l'iterazione sui dataframe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from file_reader import read_excel_files
-from report_generator import generate_text_report, generate_small_text_report
+from report_generator import generate_small_text_report, tabular_report
 #from data_analyzer import analyze_data
 from data_reorganizer import reorganize_data_merge, merge_dataframes_ordered
 from excel_writer import write_excel_file
@@ -10,8 +10,23 @@ def main(input_folder, output_filename):
 
     # Passo 2: Generazione dei report
     for filename, df in dataframes.items():
+        other = set(dataframes)
+        other.discard(filename)
+        print(other, filename)
+        filtered_dict = {key: value for key, value in dataframes.items() if key in other}
+
         report_filename = f"report_{filename}.txt"
         generate_small_text_report(df, "reports", report_filename)
+        rep = tabular_report(df)
+        
+        #print(rep,end='\n\n')
+        missing = rep[rep.columns[2]]
+        check = (missing != 0).any()
+        # Se il check e' TRUE, c'è una colonna che manca di alcuni dati.
+        # E' necessario cercarli nel resto delle tabelle.
+        # il resto delle tabelle è il dizionario filtered_dict
+        print(check)
+
 
     # Passo 3: Analisi dei dati
     """for filename, df in dataframes.items():
@@ -23,9 +38,9 @@ def main(input_folder, output_filename):
     # Passo 4: Riorganizzazione dei dati
     #reorganized_data = 
     reorganize_data_merge(dataframes)
-    merged = merge_dataframes_ordered(dataframes)
-    print(merged)
-    merged.to_csv('output.csv')
+    #merged = merge_dataframes_ordered(dataframes)
+    #print(merged)
+    #merged.to_csv('output.csv')
 
     # Passo 5: Scrittura del file Excel di output
     #write_excel_file(output_filename, reorganized_data)
